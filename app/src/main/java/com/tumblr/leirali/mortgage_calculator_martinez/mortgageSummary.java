b36408e7f24bf6bfae7f66d697f08b6ad4183640
Leirali <martinezlb@g.cofc.edu>
2017-02-13 09:30:08 -0500

    push

---
+++ app/src/main/java/com/tumblr/leirali/mortgage_calculator_martinez/mortgageSummary.java
@@ -0,0 +1,11 @@
+package com.tumblr.leirali.mortgage_calculator_martinez;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MYULAFLAGA on 2/13/2017.
+ */
+
+public class mortgageSummary extends AppCompatActivity {
+    
+}
---
+++ app/src/main/java/com/tumblr/leirali/mortgage_calculator_martinez/paymentSummary.java
@@ -0,0 +1,12 @@
+package com.tumblr.leirali.mortgage_calculator_martinez;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by MYULAFLAGA on 2/13/2017.
+ */
+
+public class paymentSummary extends AppCompatActivity {
+
+
+}
